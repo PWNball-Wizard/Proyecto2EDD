81b6e68c88594f7ef1fda36061cca18230e73a76
JS2202 <60891410+JS2202@users.noreply.github.com>
2020-05-20 19:34:38 -0500

    Metodo Elimina que faltaba en la clase NodoArbol

--- ProyectoSegundo3/src/Clases/ArbolBinario.java
+++ ProyectoSegundo3/src/Clases/ArbolBinario.java
@@ -36,6 +36,76 @@ public class ArbolBinario implements Serializable
         }
     }
 
+    public void elimina(NodoArbol r, String etq, NodoArbol arr[]) {
+
+        if (r == null) {
+            arr[0] = null;
+            arr[1] = r;
+        } else {
+            if (r.getEtq().equals(etq)) {//encontre el dato a eliminar
+
+                arr[0] = r;//porque no importa si tiene un hijo, o dos, o ninguno el dato a eliminar SIEMPRE es r
+
+                if (r.getDer() == null && r.getIzq() == null) {//Cuando no tiene hijos
+                    arr[0] = r;
+                    arr[1] = null; //para que se reconecte con un nulo
+                } else {
+                    //cuando tiene un hijo
+                    if (!(r.getDer() != null && r.getIzq() != null)) {//si los dos son nulos tiene dos hijos, y para que de un V
+                        //cuando uno de los dos de falso y el otro verdad entonces al evaluar en && e invertirse los signos 
+                        //significa que uno tiene un solo hijo
+                        //asi se evita que que entre cuando tiene 2 hijos
+                        if (r.getDer() != null) {//para ver de que lado esta el nodo hijo**para ver si esta ala derecha
+                            arr[1] = r.getDer();
+                        } else {
+                            arr[1] = r.getIzq();
+                        }
+
+                    } else {
+                        //tiene dos hijos
+                        //el sucesor enOrden es el que esta a la derecha mas a la izquierda
+                        if (r.getDer().getIzq() == null) {//si a la izquierda hay un nulo entonces toma el que esta a la derecha
+                            NodoArbol se = r.getDer();
+                            se.setIzq(r.getIzq());//conecta lo que tenia el Nodo eliminado a su izquierda, a el nodo sucesor en su izquierda
+                            r.setDer(null);
+                            r.setIzq(null);
+                            arr[1] = se;
+                        } else {
+                            NodoArbol aux = sucesorE(r.getDer());
+                            NodoArbol se = aux.getIzq();
+                            aux.setIzq(se.getDer());//aux en izquierda conecta con el derecho del sucesor en orden
+                            se.setDer(r.getDer());
+                            se.setIzq(r.getIzq());
+                            arr[1] = se;
+                        }
+                        r.setDer(null);
+                        r.setIzq(null);
+                    }
+                }
+            } else {
+                if (r.getEtq().compareTo(etq) > 0) {
+                    elimina(r.getIzq(), etq, arr);
+                    r.setIzq(arr[1]);
+                } else {
+                    elimina(r.getDer(), etq, arr);
+                    r.setDer(arr[1]);
+                }
+                arr[1] = r;
+            }
+        }
+
+    }
+
+    public NodoArbol sucesorE(NodoArbol r) {
+
+        if (r.getIzq().getIzq() != null) {
+            return sucesorE(r.getIzq());
+        } else {
+            return r;
+        }
+
+    }
+    
     /**
      * @return the r
      */
