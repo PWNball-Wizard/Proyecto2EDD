8cee1f485fc46484295b7a1e568c1e836806bd48
JS2202 <60891410+JS2202@users.noreply.github.com>
2020-05-26 20:33:59 -0500

    Contiene el nuevo chat
    A este ya le sirve casi todo, la busqueda la comente

---
+++ Proyecto con otro chat/ProyectoSegundo3/src/Clases/Multilistas.java
@@ -0,0 +1,139 @@
+package Clases;
+
+import java.io.Serializable;
+import javax.swing.JOptionPane;
+
+
+public class Multilistas implements Serializable
+{
+
+    private static Nodo mov = null;//guarda un dato eliminado para hacer el movimiento 
+    //para eliminar una lista se necesita elimnar y crear otra lista
+//*****************************************************
+    public static Nodo inserta(Nodo r, Nodo n, int nivel, String etqs[]) //nodo raiz,nodo a ingresar,nivel,etiqueta
+    {
+        if (nivel == etqs.length - 1)//VALIDA QUE ESTEMOS EN EL NIVEL CORRECTO
+        {
+            LSL ls = new LSL();//crea una nueva lista
+            ls.setR(r);//define la raiz en base a la que recibira como parametro
+            ls.inserta(n);//inserta el dato en la lista
+            r = ls.getR();//reconecta
+            return r;
+        } else
+        {
+            Nodo aux = busca(r, etqs[nivel]);//RECORRE EL ARBOL EN BUSCA DE LA ETIQUETA
+            
+            if (aux != null)
+            {
+                aux.setAbj(inserta(aux.getAbj(), n, nivel + 1, etqs));//Si encuentra el dato lo inserta
+            } else
+            {
+                System.out.println("No se encontro " + etqs[nivel] + " En el nivel " + nivel);
+            }
+            return r;
+        }
+    }
+    
+    
+
+    public static Nodo elimina(Nodo r, int nivel, String etqs[])
+    {
+        if (nivel == etqs.length - 1)
+        {
+            LSL ls = new LSL();
+            ls.setR(r);
+            Nodo aux = ls.eliminar(etqs[nivel]);
+            mov = aux;
+            if (aux == null)
+            {
+                JOptionPane.showMessageDialog(null, "No se encontro el dato", "Mensaje de Error", JOptionPane.ERROR_MESSAGE);
+            } else
+            {
+                r = ls.getR();
+                System.out.println("La lista del nivel [" + nivel + "]= " + ls.can());
+            }
+            return r;
+        } else
+        {
+            Nodo aux = busca(r, etqs[nivel]);
+            if (aux != null)
+            {
+                aux.setAbj(elimina(aux.getAbj(), nivel + 1, etqs));//recursion
+            } else
+            {
+                System.out.println("No se encontro " + etqs[nivel] + " En el nivel " + nivel);
+            }
+            return r;
+        }
+    }
+
+    public static Nodo mover(Nodo r, int nivel, String etqsE[], String etqsI[])
+    {
+        r = elimina(r, nivel, etqsE);
+        if (mov != null)
+        {
+            r = inserta(r, mov, nivel, etqsI);
+        }
+        return r;
+    }
+
+    /*public static Nodo busca(Nodo r, String etq)//METODO PARA BUSCAR DENTRO DEL ARBOL UN NODO
+    {
+        Nodo aux = null;
+        while (r != null)//Mientras no llegue al final
+        {
+            if (r.getEtq().equals(etq))//compara que sean las mismas
+            {
+                return r;
+            } else
+            {
+                r = r.getSig();//si no son iguales avanza una posicion
+            }
+        }
+        return aux;
+    }*/
+
+    public static Nodo busca(Nodo r, String etq)
+    {
+        Nodo aux = null;
+        while (r != null)
+        {
+            if (r.getEtq().equals(etq))
+            {
+                aux = r;
+                break;
+            } else
+            {
+                r = r.getSig();
+            }
+        }
+        System.out.println("EL DATO QUE ENCONTRE FUE:"+aux);
+        return aux;
+    }
+    
+    public static String desp(Nodo r, int nivel) {
+
+        String s = "";
+
+        while (r != null) {
+
+            String ss = "";
+
+            for (int i = 0; i < nivel; i++) {
+                ss = "\t";
+            }
+
+            s += ss + " nivel " + String.valueOf(nivel) + "\n";
+
+            s += ss + r.getEtq() + "\n";
+
+            s += desp(r.getAbj(), nivel + 1);
+
+            r = r.getSig();
+
+        }
+        return s;
+
+    }
+    
+}
---
+++ Proyecto con otro chat/ProyectoSegundo3/src/Clases/Nodo.java
@@ -0,0 +1,194 @@
+package Clases;
+
+import java.io.Serializable;
+
+public class Nodo implements Serializable
+{
+    /////////////////////////////////////////////////////////////
+
+    private Object obj;
+    private Object pila1;
+    private Object pila2;
+    private String etq="null"; //IDENTIFICA LO QUE HAY DENTRO DE LA CAJA
+    private Nodo sig = null;
+    private Nodo abj=null;
+    private Nodo ant = null;
+    private int tipo;
+    private Nodo arb=null;//APUNTADOR HACIA ARRIBA
+    private int num = 0;
+    
+    
+    
+    
+    public Nodo(Object obj)
+    {
+        this.obj = obj;
+    }
+
+    public Nodo(Object obj, String etq) //para la multilista
+    {
+        this.obj = obj;
+        this.etq = etq;
+    }
+    
+    public Nodo(Object obj, String etq, int num, Object pila1, Object pila2) //para la multilista
+    {
+        this.obj = obj;
+        this.etq = etq;
+        this.num = num;
+        this.pila1 = pila1;
+        this.pila2 = pila2;
+        
+    }
+
+    public Nodo(int tipo, Object obj) {//para  la cola
+        this.obj = obj;
+        this.tipo = tipo;
+    }
+    
+
+    /**
+     * @return the obj
+     */
+    public Object getObj()
+    {
+        return obj;
+    }
+
+    /**
+     * @param obj the obj to set
+     */
+    public void setObj(Object obj)
+    {
+        this.obj = obj;
+    }
+
+    /**
+     * @return the sig
+     */
+    public Nodo getSig()
+    {
+        return sig;
+    }
+
+    /**
+     * @param sig the sig to set
+     */
+    public void setSig(Nodo sig)
+    {
+        this.sig = sig;
+    }
+
+    /**
+     * @return the etq
+     */
+    public String getEtq()
+    {
+        return etq;
+    }
+
+    /**
+     * @param etq the etq to set
+     */
+    public void setEtq(String etq)
+    {
+        this.etq = etq;
+    }
+
+    /**
+     * @return the ant
+     */
+    public Nodo getAnt()
+    {
+        return ant;
+    }
+
+    /**
+     * @param ant the ant to set
+     */
+    public void setAnt(Nodo ant)
+    {
+        this.ant = ant;
+    }
+
+    /**
+     * @return the abj
+     */
+    public Nodo getAbj()
+    {
+        return abj;
+    }
+
+    /**
+     * @param abj the abj to set
+     */
+    public void setAbj(Nodo abj)
+    {
+        this.abj = abj;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public int getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public Nodo getArb() {
+        return arb;
+    }
+
+    public void setArb(Nodo arb) {
+        this.arb = arb;
+    }
+
+    /**
+     * @return the num
+     */
+    public int getNum() {
+        return num;
+    }
+
+    /**
+     * @param num the num to set
+     */
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    /**
+     * @return the pila1
+     */
+    public Object getPila1() {
+        return pila1;
+    }
+
+    /**
+     * @param pila1 the pila1 to set
+     */
+    public void setPila1(Object pila1) {
+        this.pila1 = pila1;
+    }
+
+    /**
+     * @return the pila2
+     */
+    public Object getPila2() {
+        return pila2;
+    }
+
+    /**
+     * @param pila2 the pila2 to set
+     */
+    public void setPila2(Object pila2) {
+        this.pila2 = pila2;
+    }
+    
+}
